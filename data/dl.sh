070617387bc7641fbc22002286aceff82ab66d85
Nathan Edwards <edwardsnj@gmail.com>
2024-04-12 12:27:15 -0400

    fast loading for GTEx data

---
+++ data/dl.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DATE=`date "+%Y-%m-%d"`
+wget -O sandbox-"$DATE".json 'https://sandbox.glyomics.org/api/getEnzymeMappings.php?limiter=no_filter&val='
+rm -f sandbox.json
+ln -s sandbox-"$DATE".json sandbox.json
+wget -O GTEx_gene_tpm-"$DATE".gct.gz 'https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz'
+rm -f GTEx_gene_tpm.gct.gz
+ln -s GTEx_gene_tpm-"$DATE".gct.gz GTEx_gene_tpm.gct.gz
+wget -O GTEx_SampleAttributesDS-"$DATE".txt 'https://storage.googleapis.com/adult-gtex/annotations/v8/metadata-files/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt'
+rm -f GTEx_SampleAttributesDS.txt
+ln -s GTEx_SampleAttributesDS-"$DATE".txt GTEx_SampleAttributesDS.txt
